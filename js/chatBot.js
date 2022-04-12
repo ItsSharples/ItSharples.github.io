b627d673d355dfe93fee1c3860ac51313827eb4c
ItsSharples <37125840+ItsSharples@users.noreply.github.com>
2022-04-13 00:02:50 +0100

    Two modes?

--- js/chatBot.js
+++ js/chatBot.js
@@ -1,12 +1,24 @@
 
+if(window.location.hash)
+{
+    runChat(window.location.hash.substring(1));
+}
+else
+{
+    runConfig();
+}
 
+function runConfig() {
+    document.getElementById("subbody").innerHTML = "CONFIG MODE"
+}
 
-const hash = window.location.hash.substring(1);
-var params = {}
-hash.split('&').map(hk => {
-    let temp = hk.split('=');
-    params[temp[0]] = temp[1]
-});
-console.log(params); //Here are the params to use
+function runChat(hash) {
+    var params = {}
+    hash.split('&').map(hk => {
+        let temp = hk.split('=');
+        params[temp[0]] = temp[1]
+    });
+    console.log(params); //Here are the params to use
 
-document.getElementById("subbody").innerHTML = params.access_token.toString()
+    document.getElementById("subbody").innerHTML = "Chatbot mode" + params.access_token.toString()
+}
