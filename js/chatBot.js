5681c89c0ccf57d372af9b4e79f8222b917705b5
ItsSharples <37125840+ItsSharples@users.noreply.github.com>
2022-04-12 23:58:36 +0100

    Split up hash maybe

--- js/chatBot.js
+++ js/chatBot.js
@@ -1,5 +1,12 @@
 
 
 
-const oauth = document.location.hash
-document.getElementById("subbody").innerHTML = oauth.toString()
+const hash = window.location.hash.substring(1);
+var params = {}
+hash.split('&').map(hk => {
+    let temp = hk.split('=');
+    params[temp[0]] = temp[1]
+});
+console.log(params); //Here are the params to use
+
+document.getElementById("subbody").innerHTML = params.access_token.toString()
