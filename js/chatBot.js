479b01314c80947ff4edb979bab136fcc0194736
ItsSharples <37125840+ItsSharples@users.noreply.github.com>
2022-04-13 00:17:46 +0100

    Hehehe

--- js/chatBot.js
+++ js/chatBot.js
@@ -22,3 +22,17 @@ function runChat(hash) {
 
     document.getElementById("subbody").innerHTML = "Chatbot mode" + params.access_token.toString()
 }
+
+function authenticate() {
+    const url = "https://id.twitch.tv/oauth2/authorize";
+    const client_id = "kf48fc5oafct9wqb1jf3lrsfurujq2";
+    const redirect_uri = "https://itssharples.github.io/chatbox";
+    const scope = "chat%3Aread";
+    const state = "ThisIsSpooky";
+    window.open(url + "?response_type=token" +
+        "&client_id=" + client_id +
+        "&redirect_uri=" + redirect_uri + 
+        "&scope=" + scope +
+        "&state=" + state
+    )
+}
